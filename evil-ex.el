a88cbb403cb2c2b0c6408cf39a42be8cd5b9c011
Tom Dalziel <tom_dl@hotmail.com>
2025-01-03 22:43:20 +0100

    Remove check for `beg` from evil--ex-update
    Fixes #1949

--- evil-ex.el
+++ evil-ex.el
@@ -422,7 +422,7 @@ This function is usually called from `after-change-functions'
 hook. If BEG is non-nil (which is the case when called from
 `after-change-functions'), then an error description is shown
 in case of incomplete or unknown commands."
-  (when (and beg (eq this-command #'self-insert-command))
+  (when (eq this-command #'self-insert-command)
     (let ((cmd (lookup-key evil-ex-shortcut-map (minibuffer-contents-no-properties))))
       (when (commandp cmd)
         (setq evil--ex-expression `(call-interactively #',cmd))
